8d803e7bb2dd6b2736fdcfe95d5a9a2d8eb31852
Radiyah Abdus-Sabur <rsabur58@gmail.com>
2021-02-01 20:13:07 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ def join_nested_strings(src)
     inner_index = 0
     mixed_data = ""
     while inner_index < src[row_index].count do
-      if src[row_index][inner][0] == String
+      if src[row_index][inner] == String
       mixed_data = src[row_index][inner_index]
     end
       inner +=1
