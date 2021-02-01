12e212c7bedd89df39b5545968e5f6d5eebe4097
Radiyah Abdus-Sabur <rsabur58@gmail.com>
2021-02-01 19:58:45 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ def join_nested_strings(src)
     inner = 0
     mixed_data = ""
     while inner < src[count].length
-      if src[count][inner] = string
+      if src[count][inner] = String
       new_array << src[count][inner]
     end
       inner +=1
